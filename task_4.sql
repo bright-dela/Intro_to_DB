9a598aaff0d779d321fb560ad21ffdb53ddb8cc3
bright-settor <brightdelasettor@gmail.com>
2025-08-17 12:06:29 +0000

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,10 +1,10 @@
+USE alx_book_store;
+
 SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
-AND TABLE_NAME = 'books';
+    book_id,
+    title,
+    author_id,
+    price,
+    publication_date
+FROM books
+
